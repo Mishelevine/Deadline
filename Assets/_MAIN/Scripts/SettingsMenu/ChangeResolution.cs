770822e6883b19c4632124b5a8593b055272f7a6
misha <mllevin@edu.hse.ru>
2023-06-26 03:03:21 +0500

    Разрешение

--- Assets/_MAIN/Scripts/SettingsMenu/ChangeResolution.cs
+++ Assets/_MAIN/Scripts/SettingsMenu/ChangeResolution.cs
@@ -12,9 +12,9 @@ public class ChangeResolution : MonoBehaviour
 
     public void Change()
     {
-        if (dropdown.value == 0) Screen.SetResolution(1920 , 1080,true);
-        else if (dropdown.value == 1) Screen.SetResolution(1600, 900, true);
-        else if (dropdown.value == 2) Screen.SetResolution(1366, 768, true);
-        else if (dropdown.value == 3) Screen.SetResolution(2560, 1440, true);
+        if (dropdown.value == 0) Screen.SetResolution(1920 , 1080,Screen.fullScreen);
+        else if (dropdown.value == 1) Screen.SetResolution(1600, 900, Screen.fullScreen);
+        else if (dropdown.value == 2) Screen.SetResolution(1366, 768, Screen.fullScreen);
+        else if (dropdown.value == 3) Screen.SetResolution(2560, 1440, Screen.fullScreen);
     }
 }
